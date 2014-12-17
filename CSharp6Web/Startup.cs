7936d97484e9580e7d0e6779d213bbb34534bc04
Ahmed ElSayed <ahmels@microsoft.com>
2014-12-17 14:42:22 -0800

    inital

---
+++ CSharp6Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSharp6Web.Startup))]
+namespace CSharp6Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
