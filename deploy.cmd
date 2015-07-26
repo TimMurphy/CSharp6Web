0e9c9bd346478a2fbb0341a523ff622e87a73a8a
Tim Murphy <tim@26tp.com>
2015-07-27 06:15:53 +1000

    Increased msbuild logging level to detailed.

--- deploy.cmd
+++ deploy.cmd
@@ -73,9 +73,9 @@ IF /I "CSharp6Web.sln" NEQ "" (
 
 :: 2. Build to the temporary path
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\CSharp6Web\CSharp6Web.csproj" /nologo /verbosity:n /t:Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
+  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\CSharp6Web\CSharp6Web.csproj" /nologo /verbosity:d /t:Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
 ) ELSE (
-  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\CSharp6Web\CSharp6Web.csproj" /nologo /verbosity:n /t:Build /p:AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:Solution%Dir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
+  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\CSharp6Web\CSharp6Web.csproj" /nologo /verbosity:d /t:Build /p:AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:Solution%Dir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
 )
 
 IF !ERRORLEVEL! NEQ 0 goto error
